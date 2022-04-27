9b73af8015804f94c8e69005a0186071b2bbc741
Chareef Musodee <chareefm@osd.co.th>
2022-04-27 15:43:43 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Login from './Login.js';
+import Album from './Album.js';
+import Register from './Register.js';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/register" element={<Register />}/>
+      <Route path="/album" element={<Album />}/>
+      
+    </Routes>
+  </BrowserRouter>,
+   document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
